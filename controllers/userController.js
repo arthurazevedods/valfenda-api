5795be4f22efef8f786ed2a25ef4296060904fce
arthur <arthurazevedods@gmail.com>
2024-10-29 16:01:22 -0300

    userController token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,13 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs'); 
+
+// Função para gerar token
+const generateToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+    });
+};
 
 // Registrar novo usuário
 const signUp = async (req, res) => {
@@ -11,16 +20,30 @@ const signUp = async (req, res) => {
         res.status(500).json({ message: 'Erro ao cadastrar usuário', error });
     }
 };
+
 // Login do usuário
 const login = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });
-        
+
+        // Se o usuário não for encontrado ou a senha estiver incorreta
         if (!user || !(await user.comparePassword(password))) {
             return res.status(401).json({ message: 'Credenciais inválidas' });
         }
-        res.status(200).json({ message: 'Login bem-sucedido', user });
+
+        // Cria o token
+        const token = generateToken(user._id); 
+
+        return res.json({
+            message: "Login bem-sucedido",
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+            token 
+        });
     } catch (error) {
         res.status(500).json({ message: 'Erro ao fazer login', error: error.message });
     }
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = require('./config/db');
 // Criação do app antes de usar o cors
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.use(cors({
     origin: [
